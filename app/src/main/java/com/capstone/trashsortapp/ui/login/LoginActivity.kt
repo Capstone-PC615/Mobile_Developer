85c768e20d7b77f103759ceb1f4e3fe4bb772f4e
Mats-Mingil <rohmatmingil@gmail.com>
2023-06-08 08:23:52 +0700

    fixed LoginActivity.kt

--- app/src/main/java/com/capstone/trashsortapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/capstone/trashsortapp/ui/login/LoginActivity.kt
@@ -2,16 +2,16 @@ package com.capstone.trashsortapp.ui.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.capstone.trashsortapp.R
 import com.capstone.trashsortapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
